4b6c2727d49ad542437eb8d09d2ff302475f95fd
AnnaAntonik <98764927+AnnaAntonik@users.noreply.github.com>
2022-01-31 20:38:21 +0200

    Add files via upload

---
+++ PracticalWork1/src/work12/Task12.java
@@ -0,0 +1,11 @@
+package work12;
+import java.util.Scanner;
+
+public class Task12 {
+    public static void main(String[] args) {
+        System.out.print("Enter the name:");
+        Scanner sc = new Scanner (System.in);
+        String name = sc.nextLine();
+        System.out.println("My name is" + name);
+    }
+}
